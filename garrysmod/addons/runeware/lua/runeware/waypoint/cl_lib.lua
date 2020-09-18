5002d58ad64feaa1ea9fdc92976f832f03055dbf
Tony Morris <morrantho@gmail.com>
2020-09-18 16:44:48 -0400

    Removed: More waypoint spam.

--- garrysmod/addons/runeware/lua/runeware/waypoint/cl_lib.lua
+++ garrysmod/addons/runeware/lua/runeware/waypoint/cl_lib.lua
@@ -2,49 +2,37 @@ local readstr=net.ReadString;
 local readvec=net.ReadVector;
 local hookadd=hook.Add;
 local localplayer=LocalPlayer;
-
+local camstart=cam.Start3D2D;
+local camend=cam.End3D2D;
 
 function waypoint.draw2d()
-    print("waypoint.draw2d");
-    -- local pl=LocalPlayer();
-    -- local plpos=pl:GetPos();
-    -- local waypoints=cache.get("waypoints",pl);
-    -- for title,wpos in pairs(waypoints) do
-        -- local dist=plpos:Dot(wpos)
-        -- local triangle = {
-        --     { x = 100, y = 200 },
-        --     { x = 150, y = 100 },
-        --     { x = 200, y = 200 }
-        -- }
-        -- draw.DrawText
-    -- end
+	local pl=LocalPlayer();
+	local plpos=pl:GetPos();
+	local waypoints=cache.get("waypoints",pl)||{};
+	for title,wpos in pairs(waypoints) do
+		-- idk why this code keeps vanishing, but what was here was ancient.
+	end
 end
 hookadd("HUDPaint","waypoint.draw2d",waypoint.draw2d);
 
 function waypoint.draw3d()
-    print("waypoint.draw3d");
-    -- local pl=LocalPlayer();
-    -- local waypoints=cache.get("waypoints",pl);
-    -- for title,wpos in pairs(waypoints) do
-    --     cam.Start3D2D(wpos);
-    --         surface.DrawCircle(0,0,64,Color(255,0,0));
-    --     cam.End3D2D(); 
-    -- end
+	local pl=LocalPlayer();
+	local waypoints=cache.get("waypoints",pl)||{};
+	for title,wpos in pairs(waypoints) do
+		-- idk why this code keeps vanishing, but what was here was ancient.
+	end
 end
 hookadd("PostDrawTranslucentRenderables","waypoint.draw3d",waypoint.draw3d);
 
--- 32+math.sin(CurTime())*32
 cache.register({
-    name="waypoints",
-    
-    add=function(varid,ent,cached,netdata)
-        print("WAYPOINT ASS");
-        if !cached[varid] then cached[varid]={}; end
-        cached[varid][readstr()]=readvec();
-    end,
+	name="waypoints",
+	
+	add=function(varid,ent,cached,netdata)
+		if !cached[varid] then cached[varid]={}; end
+		cached[varid][readstr()]=readvec();
+	end,
 
-    remove=function(varid,ent,cached,netdata)
-        print("WAYPOINT REMOVE");
-        cached[varid][readstr()]=nil
-    end
+	remove=function(varid,ent,cached,netdata)
+		cached[varid][readstr()]=nil
+	end
 });
--- garrysmod/addons/runeware/lua/runeware/waypoint/sv_lib.lua
+++ garrysmod/addons/runeware/lua/runeware/waypoint/sv_lib.lua
@@ -1,18 +1,18 @@
-cache.register({
-    name="waypoints",
-    
-    add=function(varid,ent,cached,netdata)
-        if !cached[varid] then cached[varid]={}; end
-        cached[varid][netdata.title]=netdata.pos;
-        net.WriteString(netdata.title);
-        net.WriteVector(netdata.pos);
-        --net.WriteColor(netdata.col);
-        -- print("WAYPOINT ADD",ent);
-    end,
+local writestr=net.WriteString;
+local writevec=net.WriteVector;
 
-    remove=function(varid,ent,cached,netdata)
-        cached[varid][netdata.title]=nil
-        net.WriteString(netdata.title)
-        -- print("WAYPOINT REMOVE",ent);
-    end
+cache.register({
+	name="waypoints",
+	
+	add=function(varid,ent,cached,netdata)
+		if !cached[varid] then cached[varid]={}; end
+		cached[varid][netdata.title]=netdata.pos;
+		writestr(netdata.title);
+		writevec(netdata.pos);
+	end,
+
+	remove=function(varid,ent,cached,netdata)
+		cached[varid][netdata.title]=nil;
+		writestr(netdata.title);
+	end
 });
