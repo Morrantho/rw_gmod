2a08a4103b576b2f3bbd11eaa880ae402d18f8f4
C-olossal <71410705+C-olossal@users.noreply.github.com>
2020-09-27 19:06:06 -0500

    changes to chat commands and added ooc and advert

--- garrysmod/addons/runeware/lua/runeware/cmd/cmds/chat/sv_global.lua
+++ garrysmod/addons/runeware/lua/runeware/cmd/cmds/chat/sv_global.lua
@@ -1,17 +1,17 @@
 if !cmd.enabled then return; end
-util.AddNetworkString("chat.ooc")
+util.AddNetworkString("chat.global")
 
 local CMD         = {};
-CMD.name          = "ooc";
-CMD.usage         = "ooc";
-CMD.description   = "Send a message in out of character chat.";
+CMD.name          = "global";
+CMD.usage         = "global";
+CMD.description   = "Send a message to everyone.";
 CMD.power         = role.member;
 
 function CMD.run(ply,args,argstr)
     if #args < 1 || args[1] == "" then cmd.help(CMD,ply); return; end
-    net.Start("chat.ooc")
+    net.Start("chat.global")
         net.WriteUInt(ply:UserID(), 7)
         net.WriteString(argstr)
     net.Broadcast() 
 end
-cmd.add(CMD,"/");
+cmd.add(CMD,"advert","ad");
