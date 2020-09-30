a702755275ef32e38072c4696a91f9ab7c962d97
LG_Legacy <LegacyMinecraft@live.com>
2020-09-30 17:27:37 -0400

    Added rob, raid cooldown.

---
+++ garrysmod/addons/runeware/lua/runeware/cmd/cmds/item/sv_rob.lua
@@ -0,0 +1,21 @@
+local CMD = {}
+
+CMD.name = "rob";
+CMD.usage = "rob";
+CMD.description = "Rob a player infront of you."
+CMD.power = role.member
+
+function CMD.run( pl )
+    if pl:isconsole() then cmd.help( CMD, pl, "Player only." ) return end
+    local targ = pl:GetEyeTrace().Entity
+    if pl.robdelay && pl.robdelay > CurTime() then err( "You can not rob for another " .. math.ceil( pl.robdelay - CurTime() ) .. "s." ) return end
+    if !targ || !IsValid( targ ) then err( "You must be looking at a player.", pl ) return end
+    if targ:getmoney() <= 0 then err( "This player does not have any money.", pl ) return end
+    local amt = math.random(50, 500)
+    targ:setmoney( targ:getmoney() - amt )
+    err( "You felt a shift in your pockets.", targ )
+    success( "You successfully robbed $" .. amt .. ".", pl )
+    pl.robdelay = CurTime() + 120
+end
+
+cmd.add(CMD)
--- garrysmod/addons/runeware/lua/runeware/cmd/cmds/raid/sv_raid.lua
+++ garrysmod/addons/runeware/lua/runeware/cmd/cmds/raid/sv_raid.lua
@@ -8,9 +8,11 @@ CMD.power = role.member
 function CMD.run( pl )
     if pl:isconsole() then cmd.help( CMD, pl, "Player only." ) return end
     local status = choose( pl:israiding(), false, true )
+    if !pl:israiding() && pl.raiddelay && pl.raiddelay > CurTime() then err( "You can not raid for " .. math.ceil( pl.raiddelay - CurTime() ) .. "s." ) return end
     local rtext = choose( pl:israiding(), "You are no longer raiding.", "You are now raiding." )
     rwplayer.setraiding( pl, status )
     success( rtext, pl )
+    if status then pl.raiddelay = CurTime() + 300 return end
 end
 
 cmd.add(CMD)
