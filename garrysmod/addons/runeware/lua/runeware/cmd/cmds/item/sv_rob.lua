9d4ed6bc0408cbdcaa7884a0b2b744d1dff65387
LG_Legacy <LegacyMinecraft@live.com>
2020-09-30 17:30:47 -0400

    Fixed missing player for error messages

--- garrysmod/addons/runeware/lua/runeware/cmd/cmds/item/sv_rob.lua
+++ garrysmod/addons/runeware/lua/runeware/cmd/cmds/item/sv_rob.lua
@@ -8,7 +8,7 @@ CMD.power = role.member
 function CMD.run( pl )
     if pl:isconsole() then cmd.help( CMD, pl, "Player only." ) return end
     local targ = pl:GetEyeTrace().Entity
-    if pl.robdelay && pl.robdelay > CurTime() then err( "You can not rob for another " .. math.ceil( pl.robdelay - CurTime() ) .. "s." ) return end
+    if pl.robdelay && pl.robdelay > CurTime() then err( "You can not rob for another " .. math.ceil( pl.robdelay - CurTime() ) .. "s.", pl ) return end
     if !targ || !IsValid( targ ) then err( "You must be looking at a player.", pl ) return end
     if targ:getmoney() <= 0 then err( "This player does not have any money.", pl ) return end
     local amt = math.random(50, 500)
--- garrysmod/addons/runeware/lua/runeware/cmd/cmds/raid/sv_raid.lua
+++ garrysmod/addons/runeware/lua/runeware/cmd/cmds/raid/sv_raid.lua
@@ -8,7 +8,7 @@ CMD.power = role.member
 function CMD.run( pl )
     if pl:isconsole() then cmd.help( CMD, pl, "Player only." ) return end
     local status = choose( pl:israiding(), false, true )
-    if !pl:israiding() && pl.raiddelay && pl.raiddelay > CurTime() then err( "You can not raid for " .. math.ceil( pl.raiddelay - CurTime() ) .. "s." ) return end
+    if !pl:israiding() && pl.raiddelay && pl.raiddelay > CurTime() then err( "You can not raid for " .. math.ceil( pl.raiddelay - CurTime() ) .. "s.", pl ) return end
     local rtext = choose( pl:israiding(), "You are no longer raiding.", "You are now raiding." )
     rwplayer.setraiding( pl, status )
     success( rtext, pl )
