141306a87346573700041cfbbb1c8004624d4564
Gregg <71410481+mono-gregg@users.noreply.github.com>
2020-10-12 22:30:24 -0500

    Delete cl_hud_health.lua
    being revised.

--- garrysmod/addons/runeware/lua/runeware/ui/hud/cl_hud_health.lua
+++
@@ -1,9 +0,0 @@
-local PANEL = {}
-
-function PANEL:CalcValues()
-    local player = LocalPlayer()
-    self.value = player:Health()
-    self.max = player:GetMaxHealth() 
-end
-
-ui.add("cl_ui_health", PANEL, "cl_bar_base")
